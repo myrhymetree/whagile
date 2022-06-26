87e7a5b45205473dae1cee331e679263cdf547ad
Minjoo Jang <miinjoojang@gmail.com>
2022-06-26 23:47:34 +0900

    [FEAT] 메인 헤더 추가 진행 중

--- client/src/components/commons/MainHeader.js
+++ client/src/components/commons/MainHeader.js
@@ -1,9 +1,37 @@
+import MainHeaderCSS from './MainHeader.module.css';
+import { Button } from 'primereact/button';
+
 function MainHeader() {
 
     console.log(window.localStorage.getItem('access_token'));
 
     return (
-        <h1>헤더</h1>
+        <header>
+            <div>
+                <div id={ MainHeaderCSS.logo }>
+                    <img 
+                        src={process.env.PUBLIC_URL + '/logo_main.png'} 
+                        alt="logo_main"
+                        width="160"
+                    />                
+                </div>
+            </div>
+            <div>
+                요금제사용중
+            </div>
+            <div>
+                <div id={ MainHeaderCSS.welcome }>
+                    <span>{ }님 환영합니다</span>
+                    <div>
+                        <Button 
+                            icon="pi pi-user" 
+                            className="p-button-rounded p-button-outlined" 
+                            aria-label="User"
+                        />
+                    </div>
+                </div>
+            </div>
+        </header>
     );
 }
 
