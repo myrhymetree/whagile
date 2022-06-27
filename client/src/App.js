8a9b89a592841aa2364ec377104c37e5e6874164
DESKTOP-D5A9GEB\CHA WUJIN <gustlf063@gmail.com>
2022-06-27 22:22:13 +0900

    [FEAT] 칸반보드 초기 구성 커밋

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/login/Login';
-import Signup from './pages/login/Signup';
-import MainLayout from './layouts/MainLayout';
-import Main from './pages/main/Main';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Signup from "./pages/login/Signup";
+import MainLayout from "./layouts/MainLayout";
+import Main from "./pages/main/Main";
+import Kanban from "./pages/kanban/Kanban";
 // import "./assets/themes/soho/soho-dark/theme.css";  //theme
 import "./assets/themes/soho/soho-dark/theme.scss"; //theme
 // require('./assets/themes/soho/soho-dark/theme.scss');  //theme
-import "primereact/resources/primereact.min.css";   //core css
-import "primeicons/primeicons.css";                 //icons
+import "primereact/resources/primereact.min.css"; //core css
+import "primeicons/primeicons.css"; //icons
 import "./App.css";
 
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={ <Login/>} />
-          <Route path="/signup" element={ <Signup/> } />
-          <Route path="/main" element={ <MainLayout/>} >
-            <Route index element={ <Main/> } />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/main" element={<MainLayout />}>
+          <Route path="kanban" element={<Kanban />} />
+          <Route index element={<Main />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
