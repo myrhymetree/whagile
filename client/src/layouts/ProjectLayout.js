b60323b633856bba7d05a6832975ada381846d2c
Minjoo Jang <miinjoojang@gmail.com>
2022-06-30 23:50:47 +0900

    [DESIGN] css 오류 해결
    page title css 깨짐현상을 수정하였습니다.

--- client/src/layouts/ProjectLayout.js
+++ client/src/layouts/ProjectLayout.js
@@ -1,16 +1,18 @@
 import { Outlet } from 'react-router-dom';
 import MainHeader from '../components/commons/MainHeader';
 import MainNavbar from '../components/commons/MainNavbar';
-import MainLayoutCSS from './MainLayout.module.css';
+import ProjectLayoutCSS from './ProjectLayout.module.css';
 
 function ProjectLayout() {
 
     return (
         <div>
             <MainHeader/>
-            <div className={ MainLayoutCSS.layout }>
+            <div className={ ProjectLayoutCSS.layout }>
                 <MainNavbar/>
-                <Outlet/>
+                <main className={ ProjectLayoutCSS.main }>
+                    <Outlet/>
+                </main>
             </div>
         </div>
     );
