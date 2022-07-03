dd789a446a8a0ac77c46c34ea24e0f9d9e57ecde
DESKTOP-HO9HPE1\hoyamiya <dlghtjdsla99@gmail.com>
2022-07-03 15:07:06 +0900

    [FEAT] 마이페이지, 아이디찾기, 비밀번호 찾기 구현

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -4,8 +4,6 @@ import { useForm, Controller, FieldError } from 'react-hook-form';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import { Password } from 'primereact/password';
-import { Dialog } from 'primereact/dialog';
-import { Divider } from 'primereact/divider';
 import { classNames } from 'primereact/utils';
 import { Toast } from 'primereact/toast';
 import './Login.css';
@@ -15,6 +13,9 @@ function Login() {
     // const [id, setId] = useState("");
     // const [password, setPassword] = useState("");
     const [isLogin, setIsLogin] = useState('');
+    const [searchIDForm, setSearchIDForm] = useState(false);
+    const [loginform, setLoginform] = useState(true);
+    const [searchPwForm, setSearchPwForm] = useState(false);
 
     useEffect(() => {
         console.log("useEffect Login", isLogin);
@@ -28,7 +29,11 @@ function Login() {
     const navigate = useNavigate();
     const defaultValues = {
         memberId : '',
-        password : ''
+        password : '',
+        email: '',
+        searchIDEmail: '',
+        searchPWMemberId: '',
+        searchPWEmail: '',
     };
 
     const toast = useRef(null);
@@ -36,12 +41,43 @@ function Login() {
         toast.current.show({severity:'error', summary: 'Error Message', detail:msg, life: 3000});
     }
 
+    const showSuccess = (msg) => {
+        toast.current.show({severity:'success', summary: 'Success Message', detail:msg, life: 3000});
+    }
+
     const clear = () => {
         toast.current.clear();
     }
 
+    const getFormErrorMessage = (name) => {
+        return errors[name] && <small className="p-error">{errors[name].message}</small>
+    };
 
     const { control, formState: { errors }, handleSubmit } = useForm({ defaultValues });
+
+    const onClickRegister = () => {
+        navigate('/signup') 
+    };
+
+    const onClickLogin = () => {
+        setLoginform(true);
+        setSearchPwForm(false);
+        setSearchIDForm(false);
+    }
+
+    const onClickSearchID = () => {
+        setLoginform(false);
+        setSearchPwForm(false);
+        setSearchIDForm(true);
+    };
+
+    const onClickSearchPW = () => {
+        setLoginform(false);
+        setSearchPwForm(true);
+        setSearchIDForm(false);
+    };
+
+
 
     const onSubmitHandler = (data) => {
         
@@ -55,14 +91,14 @@ function Login() {
         .then(response => response.json())
         .then(json => {
 
-            console.log(json);
-            if(json.status == 200){
+            console.log('Login', json);
+            if(json.status == 200){                
                 window.sessionStorage.setItem('isLogin', true);
                 setIsLogin(true);
 
                 window.localStorage.setItem('access_token', json.accessToken);
                 window.localStorage.getItem('access_token') !== 'undefined' 
-                ? navigate('/project') 
+                ? ((json.result[0].role == 'ROLE_ADMIN')? navigate('/admin') : navigate('/project'))
                 : showError('로그인에 실패하였습니다.');
             } else {
                 showError('로그인에 실패하였습니다.');
@@ -78,72 +114,270 @@ function Login() {
         });
     };
 
-    const getFormErrorMessage = (name) => {
-        return errors[name] && <small className="p-error">{errors[name].message}</small>
-    };
+    const onSearchIDHandler = (data) => {
+        console.log(data);
+        
+        fetch(`http://localhost:8888/api/account/search?email=${data.searchIDEmail}`, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            }
+        })
+        .then(response => response.json())
+        .then(json => {
+
+            console.log(json);
+            if(json.status == 200){
+                showSuccess('이메일에서 아이디를 확인해주세요.');
+                onClickLogin();
+            } else {
+                showError('ID 찾기 기능이 실패하였습니다.');
+            }            
+
+            // ? ((data.memberId === 'admin')? navigate('/admin') : navigate('/main'))
+            // : console.log('login Failed');         
+
+        })
+        .catch((err) => {
+          console.log('login error: ' + err);
+          showError('ID 찾기 기능이 실패하였습니다.');
+        });
+
+
+    }
+
+
+
+    const onSearchPWHandler = (data) => {
+        console.log(data);
+
+        fetch(`http://localhost:8888/api/account/temppwd?id=${data.searchPWMemberId}&email=${data.searchPWEmail}`, {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            }
+        })
+        .then(response => response.json())
+        .then(json => {
+
+            console.log(json);
+            if(json.status == 200){
+                showSuccess('이메일에서 임시비밀번호를 확인해주세요.');
+                onClickLogin();
+            } else {
+                showError('비밀번호 찾기 기능이 실패하였습니다.');
+            }                   
+
+        })
+        .catch((err) => {
+          console.log('login error: ' + err);
+          showError('비밀번호 찾기 기능이 실패하였습니다.');
+        });
+
+    }
+
 
     return (
         <div className="flex justify-content-center login-center">
             <Toast ref={toast} position="top-center" />
+            { loginform && 
+                <div className='formDiv'>
+                    <div className='card'>
+                        <h5 className="text-center">로그인</h5>
+                        <form 
+                            style={{display: 'flex', flexDirection: 'column' }}
+                            onSubmit={handleSubmit(onSubmitHandler)}
+                            className="p-fluid"
+                        >
+                            <div className="field">
+                                <span className="p-float-label">
+                                    <Controller 
+                                        name="memberId" 
+                                        control={control} 
+                                        rules={{ required: '아이디는 필수값입니다.' }} 
+                                        render={({ field, fieldState }) => (
+                                            <InputText 
+                                                id={field.name} 
+                                                {...field}
+                                                autoComplete="off" 
+                                                autoFocus 
+                                                className={classNames({ 'p-invalid': fieldState.invalid })} 
+                                            />
+                                    )} />
+                                    <label htmlFor="memberId" className={classNames({ 'p-error': errors.memberId })}>ID*</label>
+                                </span>
+                                {getFormErrorMessage('memberId')}
+                            </div>
 
-            <div className='formDiv'>
-                <div className='card'>
-                    <h5 className="text-center">로그인</h5>
-                    <form 
-                        style={{display: 'flex', flexDirection: 'column' }}
-                        onSubmit={handleSubmit(onSubmitHandler)}
-                        className="p-fluid"
-                    >
+                            <div className="field">
+                                <span className="p-float-label">
+                                    <Controller 
+                                        name="password" 
+                                        control={control} 
+                                        rules={{ required: '비밀번호는 필수값입니다.' }} 
+                                        render={({ field, fieldState }) => (
+                                            <Password 
+                                                id={field.name} 
+                                                {...field} 
+                                                toggleMask 
+                                                autoComplete="off"
+                                                className={classNames({ 'p-invalid': fieldState.invalid })} 
+                                            />
+                                    )} />
+                                    <label htmlFor="password" className={classNames({ 'p-error': errors.password })}>Password*</label>
+                                </span>
+                                {getFormErrorMessage('password')}
+                            </div>
+
+                            <Button 
+                                type="submit"
+                                label="로그인"  
+                                className="p-button-success p-button-sm" 
+                            />
+                        </form>
+                        <Button 
+                            style={{ width: '34%' }} 
+                            className="p-button-warning p-button-text" 
+                            label="아이디찾기" 
+                            onClick={ onClickSearchID }
+                        />            
+                        <Button 
+                            style={{ width: '38%' }} 
+                            className="p-button-warning p-button-text" 
+                            label="비밀번호찾기" 
+                            onClick={ onClickSearchPW }
+                        />
+                        <Button 
+                            style={{ width: '28%' }} 
+                            className="p-button-warning p-button-text" 
+                            label="회원가입" 
+                            onClick={ onClickRegister}
+                        />            
+                        {/* <NavLink to="/signup">회원가입</NavLink> */}
+                    </div>
+                </div>
+            }
+
+
+
+
+
+            { searchIDForm && 
+                <div className='formDiv'>
+                    <div className='card'>
+                        <h5 className="text-center">아이디 찾기</h5>
+                        <form 
+                            style={{display: 'flex', flexDirection: 'column' }}
+                            onSubmit={handleSubmit(onSearchIDHandler)}
+                            className="p-fluid"
+                        >
+
+                        <div className="field">
+                            <span className="p-float-label p-input-icon-right">
+                                <i className="pi pi-envelope" />
+                                <Controller name="searchIDEmail" control={control}
+                                    rules={{ required: 'Email은 필수값입니다.', 
+                                            pattern: 
+                                            { 
+                                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, 
+                                                message: '이메일 주소가 올바르지 않습니다. E.g. example@email.com' 
+                                            }
+                                        }}
+                                    render={({ field, fieldState }) => (
+                                        <InputText id={field.name} {...field} autoComplete="off" className={classNames({ 'p-invalid': fieldState.invalid })} />
+                                )} />
+                                <label htmlFor="searchIDEmail" className={classNames({ 'p-error': !!errors.searchIDEmail })}>이메일*</label>
+                            </span>
+                            {getFormErrorMessage('searchIDEmail')}
+                        </div>
+
+                        <Button 
+                            type="submit"
+                            label="이메일로 아이디 받기"  
+                            className="p-button-success p-button-sm"  
+                        />
+                        </form>
+                        <Button 
+                            style={{ width: '100%' }} 
+                            className="p-button-warning p-button-text" 
+                            label="로그인 페이지로" 
+                            onClick={ onClickLogin }
+                        />            
+                    </div>
+                </div>
+            }
+
+
+
+
+
+            { searchPwForm && 
+                <div className='formDiv'>
+                    <div className='card'>
+                        <h5 className="text-center">비밀번호 찾기</h5>
+                        <form 
+                            style={{display: 'flex', flexDirection: 'column' }}
+                            onSubmit={handleSubmit(onSearchPWHandler)}
+                            className="p-fluid"
+                        >
+
                         <div className="field">
                             <span className="p-float-label">
                                 <Controller 
-                                    name="memberId" 
+                                    name="searchPWMemberId" 
                                     control={control} 
                                     rules={{ required: '아이디는 필수값입니다.' }} 
                                     render={({ field, fieldState }) => (
                                         <InputText 
                                             id={field.name} 
                                             {...field}
-                                            autoComplete="off" 
-                                            autoFocus 
+                                            autoComplete="off"                                          
                                             className={classNames({ 'p-invalid': fieldState.invalid })} 
                                         />
                                 )} />
-                                <label htmlFor="memberId" className={classNames({ 'p-error': errors.memberId })}>ID*</label>
+                                <label htmlFor="searchPWMemberId" className={classNames({ 'p-error': errors.searchPWMemberId })}>ID*</label>
                             </span>
-                            {getFormErrorMessage('memberId')}
+                            {getFormErrorMessage('searchPWMemberId')}
                         </div>
 
                         <div className="field">
-                            <span className="p-float-label">
-                                <Controller 
-                                    name="password" 
-                                    control={control} 
-                                    rules={{ required: '비밀번호는 필수값입니다.' }} 
+                            <span className="p-float-label p-input-icon-right">
+                                <i className="pi pi-envelope" />
+                                <Controller name="searchPWEmail" control={control}
+                                    rules={{ required: 'Email 필수값입니다.', 
+                                            pattern: 
+                                            { 
+                                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, 
+                                                message: '이메일 주소가 올바르지 않습니다. E.g. example@email.com' 
+                                            }
+                                        }}
                                     render={({ field, fieldState }) => (
-                                        <Password 
-                                            id={field.name} 
-                                            {...field} 
-                                            toggleMask 
-                                            autoComplete="off"
-                                            className={classNames({ 'p-invalid': fieldState.invalid })} 
-                                        />
+                                        <InputText 
+                                            id={field.name} {...field} 
+                                            autoComplete="off" 
+                                            className={classNames({ 'p-invalid': fieldState.invalid })} />
                                 )} />
-                                <label htmlFor="password" className={classNames({ 'p-error': errors.password })}>Password*</label>
+                                <label htmlFor="emasearchPWEmailil" className={classNames({ 'p-error': !!errors.searchPWEmail })}>이메일*</label>
                             </span>
-                            {getFormErrorMessage('password')}
+                            {getFormErrorMessage('searchPWEmail')}
                         </div>
 
                         <Button 
-                            type="submit"
-                            label="로그인"  
+                            label="이메일로 임시비밀번호 받기"  
                             className="p-button-success p-button-sm"  
+                            // onClick={ onClickTempPwd }
+                            type="submit"
                         />
-                    </form>
-
-                    <NavLink to="/signup">회원가입</NavLink>
+                        </form>
+                        <Button 
+                            style={{ width: '100%' }} 
+                            className="p-button-warning p-button-text" 
+                            label="로그인 페이지로" 
+                            onClick={ onClickLogin }
+                        />            
+                    </div>
                 </div>
-            </div>
+            }            
         </div>
     );
 }
