7daf30330aa749b3171cd416ce1dc3758f323db8
DESKTOP-D5A9GEB\CHA WUJIN <gustlf063@gmail.com>
2022-07-06 00:45:49 +0900

    [FIX] 수정

--- client/src/modules/index.js
+++ client/src/modules/index.js
@@ -3,22 +3,6 @@ import authsReducer from './AuthsModule';
 import authReducer from './AuthModule';
 import authOrderReducer from './AuthOrderModule';
 import backlogReducer from './BacklogModule';
-<<<<<<< HEAD
-import taskReducer from './TaskModule';
-import cards from '../components/items/kanban/reducers/card_reducer';
-import modal from '../components/items/kanban/reducers/modal_reducer';
-import draft from '../components/items/kanban/reducers/draft_reducer';
-
-const rootReducer = combineReducers({
-  authReducer,
-  authsReducer,
-  taskReducer,
-  authOrderReducer,
-  backlogReducer,
-  cards,
-  modal,
-  draft,
-=======
 import projectsReducer from './ProjectModules';
 
 const rootReducer = combineReducers({
@@ -27,7 +11,6 @@ const rootReducer = combineReducers({
     authOrderReducer,
     backlogReducer,
     projectsReducer
->>>>>>> 04e74ad9854feeea9d740be79f1c036bad550cf0
 });
 
 export default rootReducer;
