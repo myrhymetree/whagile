0a273d648eb0256a9380ebce9b9487ebb5095c09
Minjoo Jang <miinjoojang@gmail.com>
2022-07-05 11:35:28 +0900

    [FIX] 백로그 및 스프린트 페이지 컴포넌트 수정
    렌더링 될 컴포넌트와 api요청 시 전송할 데이터를 수정하였습니다.

--- client/src/components/items/backlog-and-sprint/BacklogCreationModal.js
+++ client/src/components/items/backlog-and-sprint/BacklogCreationModal.js
@@ -1,6 +1,11 @@
 import BacklogModalsCSS from './BacklogModals.module.css';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { INSERT_BACKLOG } from '../../../modules/BacklogModule';
+
+import { callGetBacklogsAPI } from '../../../apis/BacklogAPICalls';
+/* 스프린트 목록 조회 api도 임포트해야함 */
 
 import { Button } from 'primereact/button';
 import { Dialog } from 'primereact/dialog';
@@ -9,74 +14,95 @@ import { InputText } from 'primereact/inputtext';
 import { InputTextarea } from 'primereact/inputtextarea';
 
 function BacklogCreationModal() {
-    
+
+    const dispatch = useDispatch();
+
+    /* state 정의 */
     const [displayDialog, setDisplayDialog] = useState(false);
     const [position, setPosition] = useState('center');
-    
-    const [title, setTitle] = useState(null);
-    const [description, setDescription] = useState(null);
 
-    const [selectedCategory, setSelectedCategory] = useState(null);
-    const [selectedProgressStatus, sstSelectedProgressStatus] = useState(null);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [selectedProgressStatus, setSelectedProgressStatus] = useState(null);
     const [selectedUrgency, setSelectedUrgency] = useState(null);
+    const [selectedIssue, setSelectedIssue] = useState(null);
 
-    const dialogFuncMap = {
-        'displayDialog': setDisplayDialog
-    }
-
-    const categoryOptions = [
-        { name: '기본', value: 'NY' },
-        { name: '이슈', value: 'PRS' }
-    ];
+    /* dropdown 옵션 정의 */
     const progressStatusOptions = [
-        { name: '진행 전', value: 'NY' },
-        { name: '진행 중', value: 'RM' },
-        { name: '완료', value: 'LDN' }
+        { name: '진행 전', value: '진행 전' },
+        { name: '진행 중', value: '진행 중' },
+        { name: '완료', value: '완료' }
     ];
     const urgencyOptions = [
-        { name: '낮음', value: 'NY' },
-        { name: '보통', value: 'RM' },
-        { name: '긴급', value: 'LDN' }
+        { name: '낮음', value: '낮음' },
+        { name: '보통', value: '보통' },
+        { name: '긴급', value: '긴급' }
     ];
-    
-    const onCategoryChange = (e) => {
-        setSelectedCategory(e.value);
-    }
+    const issueCheck = [
+        { name: '기본', value: 0 },
+        { name: '이슈', value: 1 }
+    ];
 
-    const onProgressStatusChange = (e) => {
-        sstSelectedProgressStatus(e.value);
-    }
+    const newBacklog = {
+        title: title,
+        description: description,
+        progressStatus: selectedProgressStatus,
+        urgency: selectedUrgency,
+        issue: selectedIssue
+    };
 
-    const onUrgencyChange = (e) => {
-        setSelectedUrgency(e.value);
-    }
-
-    const onClick = (name, position) => {
-        dialogFuncMap[`${name}`](true);
+    /* 다이얼로그 표시 */
+    const onClick = (displayDialog, position) => {
+        setDisplayDialog(true);
 
         if (position) {
             setPosition(position);
         }
     }
 
-    const onHide = (name) => {
-        dialogFuncMap[`${name}`](false);
+    /* 다이얼로그 닫기 */
+    const onHide = (displayDialog) => {
+       setDisplayDialog(false);
+       /* 다이얼로그 창이 닫힐 때 저장된 input state를 초기화한다. */
+       setTitle('');
+       setDescription('');
+       setSelectedProgressStatus(null);
+       setSelectedUrgency(null);
+       setSelectedIssue(null);
     }
 
-    const renderFooter = (name) => {
+    /* 백로그 생성 API 요청 */
+    const registNewBacklog = () => {
+        /* 필수입력사항 기입 여부 확인 */
+        if(title == '' || selectedProgressStatus == null || selectedUrgency == null || selectedIssue == null) {
+            alert('필수 입력사항을 모두 입력해주세요.');
+
+        } else {
+            fetch('http://localhost:8888/api/backlogs', {
+                method: 'POST',
+
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Token': window.localStorage.getItem('access_token')
+                },
+                body: JSON.stringify(newBacklog)
+            }).then((res) => res.json())
+            .then((result) => console.log(result));
+        };
+    };
+
+    const renderFooter = (displayDialog) => {
         return (
             <div>
                 <Button 
                     id={ BacklogModalsCSS.createBtn }
                     label="만들기" 
-                    icon="pi pi-check" 
-                    onClick={() => onHide(name)} autoFocus 
+                    onClick={ () => registNewBacklog(newBacklog) }
                     />
                 <Button 
                     id={ BacklogModalsCSS.cancelBtn }
                     label="취소" 
-                    icon="pi pi-times" 
-                    onClick={() => onHide(name)} 
+                    onClick={ () => onHide(displayDialog) } 
                 />
             </div>
         );
@@ -88,29 +114,54 @@ function BacklogCreationModal() {
                 id={ BacklogModalsCSS.backlogCreationBtn } 
                 label="백로그 추가하기" 
                 icon="pi pi-plus" 
-                onClick={() => onClick('displayDialog')} 
+                onClick={ () => onClick(displayDialog) } 
             />
             <Dialog 
                 id={ BacklogModalsCSS.backlogCreationDialog }
                 header="백로그 생성" 
                 visible={ displayDialog } 
                 style={{ width: '30vw' }} 
-                footer={ renderFooter('displayDialog') } 
-                onHide={ () => onHide('displayDialog') }
+                footer={ renderFooter(displayDialog) } 
+                onHide={ () => onHide(displayDialog) }
             >
                 <div className="field">
                     <label className="block">제목*</label><br/>
                     <InputText 
                         id={ BacklogModalsCSS.inputTitle } 
-                        aria-describedby="inputTitle-help" 
                         className="block"
-                        required 
                         value={ title }
+                        onChange={ (e) => setTitle(e.target.value) }
                     />
-                    <br/>
-                    <small id="inputTitle-help" className="p-error block">필수 입력 사항입니다.</small>
                     <br/><br/>
                 </div>
+                <div id={ BacklogModalsCSS.dropdownArea }>
+                    <Dropdown 
+                        value={ selectedProgressStatus } 
+                        options={ progressStatusOptions } 
+                        optionLabel="name" 
+                        optionValue="value" 
+                        onChange={ (e) => setSelectedProgressStatus(e.target.value) }  
+                        placeholder="진행상태*" 
+                    />  
+                    <Dropdown 
+                        value={ selectedUrgency } 
+                        options={ urgencyOptions } 
+                        onChange={ (e) => setSelectedUrgency(e.target.value) }  
+                        optionLabel="name" 
+                        optionValue="value" 
+                        placeholder="긴급도*" 
+                    />
+                    <Dropdown 
+                        value={ selectedIssue } 
+                        options={ issueCheck } 
+                        optionLabel="name" 
+                        optionValue="value" 
+                        onChange={ (e) => setSelectedIssue(e.target.value) } 
+                        placeholder="분류*" 
+                    />
+                    <br/><br/>
+                </div>
+                <br/>
                 <label>설명</label><br/>
                 <InputTextarea 
                     id={ BacklogModalsCSS.inputDescription }
@@ -119,32 +170,18 @@ function BacklogCreationModal() {
                     rows={ 5 } 
                 />
                 <br/><br/>
-                <div id={ BacklogModalsCSS.dropdownArea }>
-                    <Dropdown 
-                        value={ selectedCategory } 
-                        options={ categoryOptions } 
-                        optionLabel="name" 
-                        optionValue="value" 
-                        onChange={ onCategoryChange } 
-                        placeholder="카테고리" 
-                        />
-                    <Dropdown 
-                        value={ selectedProgressStatus } 
-                        options={ progressStatusOptions } 
-                        optionLabel="name" 
-                        optionValue="value" 
-                        onChange={ onProgressStatusChange } 
-                        placeholder="진행상태" 
-                        />
-                    <Dropdown 
-                        value={ selectedUrgency } 
-                        options={ urgencyOptions } 
-                        onChange={ onUrgencyChange } 
-                        optionLabel="name" 
-                        optionValue="value" 
-                        placeholder="긴급도" 
-                    />
-                    <br/><br/>
+                <div
+                    style={ 
+                        {
+                            display: title == '' || selectedIssue == null || selectedProgressStatus == null || selectedUrgency == null? 'block' : 'none'
+                        } 
+                    }
+                >
+                    <small 
+                        className="p-error block"
+                        >
+                        *은 필수 입력 사항입니다.
+                    </small>
                 </div>
             </Dialog>
         </>
