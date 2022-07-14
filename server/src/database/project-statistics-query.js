90bc4f91fceb7ae51d3b080bc1ca8850ed0def9c
sungjoon <myrhymetree@14.40.27.124>
2022-07-14 19:14:35 +0900

    [FEAT] 프로젝트 통계
    일감 상태별 개수 조회 쿼리문

--- server/src/database/project-statistics-query.js
+++ server/src/database/project-statistics-query.js
@@ -1,5 +1,5 @@
   /* 프로젝트 별 진행상태에 따른 일감 개수 조회 */
-  exports.selectContingTaskByProgressStatus = () => {
+  exports.selectContingTaskByProgressStatus = (projectCode) => {
     
     return `
         SELECT
@@ -9,7 +9,7 @@
                 WHERE BK.BACKLOG_CATEGORY = '일감'
                   AND BK.BACKLOG_PROGRESS_STATUS = '진행전'
                   AND BK.BACKLOG_DELETED_YN = 'N'
-                  AND BK.PROJECT_CODE = 1
+                  AND BK.PROJECT_CODE = ${ projectCode }
               ) PENDING_TASK
             , (SELECT
                       COUNT(BK.BACKLOG_CODE)
@@ -17,7 +17,7 @@
                 WHERE BK.BACKLOG_CATEGORY = '일감'
                   AND BK.BACKLOG_PROGRESS_STATUS = '진행중'
                   AND BK.BACKLOG_DELETED_YN = 'N'
-                  AND BK.PROJECT_CODE = 1
+                  AND BK.PROJECT_CODE = ${ projectCode }
               ) PROGRESSING_TASK
             , (SELECT
                       COUNT(BK.BACKLOG_CODE)
@@ -25,14 +25,14 @@
                 WHERE BK.BACKLOG_CATEGORY = '일감'
                   AND BK.BACKLOG_PROGRESS_STATUS = '완료'
                   AND BK.BACKLOG_DELETED_YN = 'N'
-                  AND BK.PROJECT_CODE = 1
+                  AND BK.PROJECT_CODE = ${ projectCode }
               ) COMPLETED_TASK
             , (SELECT 
                       COUNT(BK.BACKLOG_CODE)
                 FROM TBL_BACKLOG BK
                 WHERE BK.BACKLOG_CATEGORY = '일감'
                   AND BK.BACKLOG_DELETED_YN = 'N'
-                  AND BK.PROJECT_CODE = 1
+                  AND BK.PROJECT_CODE = ${ projectCode }
               ) TOTAL_TASK;
     `;
   }
