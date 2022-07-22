e3607132aacd420f333eab1ecb68787048f5b4fb
Minjoo Jang <miinjoojang@gmail.com>
2022-07-23 03:22:33 +0900

    [DESIGN] 컴포넌트 분리
    목록조회 영역을 관리자페이지와 사용자페이지에서 재사용할 목적으로
    문의 목록 컴포넌트를 별도의 파일로 분류하였습니다.

--- client/src/pages/inquiry/Inquiry.js
+++ client/src/pages/inquiry/Inquiry.js
@@ -1,14 +1,13 @@
 import InquiryCSS from './Inquiry.module.css';
 
 import { useEffect, useState } from 'react';
-import { callGetInquiriesAPI, callGetInquiryDetailAPI } from '../../apis/InquiryAPICalls';
+import { useDispatch, useSelector } from 'react-redux';
+import { callGetInquiriesAPI } from '../../apis/InquiryAPICalls';
 
 import MainHeader from '../../components/commons/MainHeader';
 import PageTitle from '../../components/items/PageTitle';
-import { InputText } from 'primereact/inputtext';
+import InquiryTable from '../../components/items/inquiry/InquiryTable';
 import InquiryCreationModal from './InquiryCreationModal';
-import InquiryDetailModal from './InquiryDetailModal';
-import { useDispatch, useSelector } from 'react-redux';
 import { Navigate } from 'react-router';
 
 function Inquiry() {
@@ -16,11 +15,6 @@ function Inquiry() {
     const dispatch = useDispatch();
     const inquiries = useSelector(state => state.inquiriesReducer);
     const inquiry = useSelector(state => state.inquiryReducer);
-
-    /* 페이징 및 검색 조건 */
-    const [offset, setOffset] = useState(0);
-    const [filter, setFilter] = useState('');
-    const [searchValue, setSearchValue] = useState('');
 
     const [visibleCreation, setVisibleCreation] = useState(false);
 
@@ -44,20 +38,6 @@ function Inquiry() {
         []
     );
 
-    /* 필터 조건 부여하여 목록 조회 */
-    const getFilteredList = (filteredValue) => {
-        setFilter(filteredValue);
-
-        const params = {
-            offset: 0,
-            limit: 10,
-            filter: filter
-        }
-
-        console.log(params)
-        dispatch(callGetInquiriesAPI(params));
-    }
-
     return (
         <>
             <MainHeader/>
@@ -66,63 +46,7 @@ function Inquiry() {
                     icon={ <i className="pi pi-fw pi-envelope"></i> }
                     text="1:1 문의"
                 />
-                <div id={ InquiryCSS.main }>
-                    <div>
-                        문의 목록
-                    </div>
-                        {/* <div id={ InquiryCSS.searchBar }>
-                            <label>제목 검색</label> 
-                            <InputText
-                                id="searchValue"
-                                value={searchValue}
-                                onChange={(e) => setSearchValue(e.target.value)}
-                            />
-                        </div> */}
-                    <div id={ InquiryCSS.filter }>
-                        필터 
-                        <button
-                            onClick={ () => getFilteredList('') }
-                        >
-                            전체
-                        </button>
-                        <button
-                            onClick={ () => getFilteredList('N') }
-                        >
-                            미답변
-                        </button>
-                        <button
-                            onClick={ () => getFilteredList('Y') }
-                        >
-                            답변완료
-                        </button>
-                    </div>
-                    <div id={ InquiryCSS.tableHeader }>
-                        <div>번호</div>
-                        <div>제목</div>
-                        <div>작성시간</div>
-                        <div>작성자</div>
-                        <div>문의유형</div>
-                        <div>답변상태</div>
-                        <div>자세히</div>
-                    </div>
-                    { 
-                        inquiries.length > 0
-                        ? inquiries.map(inquiry => 
-                            <div key={ inquiry.inquiryCode } className={ InquiryCSS.tableRow }>
-                                <div>{ inquiry.inquiryCode }</div>
-                                <div>{ inquiry.title }</div>
-                                <div>{ inquiry.createdDate }</div>
-                                <div>{ inquiry.memberName }</div>
-                                <div>{ inquiry.categoryName }</div>
-                                <div>{ inquiry.answeredYN === 'N'? '미답변' : '답변완료' }</div>
-                                <div>
-                                    <InquiryDetailModal inquiry={ inquiry }/>
-                                </div>
-                            </div>
-                        ) 
-                        : <div id={ InquiryCSS.noInquiry }>등록된 문의 글이 없습니다.</div>
-                    }
-                </div>
+                <InquiryTable inquiries={ inquiries }/>
                 <InquiryCreationModal 
                     visibleCreation={ visibleCreation }
                     setVisibleCreation={ setVisibleCreation }
