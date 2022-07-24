bf521864d84364bc6fad1234831f455e0597dd8e
성준 <myrhymetree@gmail.com>
2022-07-24 10:06:36 +0900

    [HOTFIX] 이메일 링크 수정
    신규  회원 초대 이메일 링크 수정

--- server/src/util/email-utils.js
+++ server/src/util/email-utils.js
@@ -242,7 +242,7 @@ exports.sendInvitationMailToNewMember = async (email, projectInfo) => {
       ${ projectInfo.projectName }에 초대 되셨습니다.<br/>
       <br/>
       아래 링크를 클릭하시고 회원 가입을 하시면 해당 프로젝트에 초대됩니다.<br/>
-      <a href="http://${process.env.REACT_APP_RESTAPI_IP}:3000/invitedMemberSignup/${ projectInfo.projectCode }/${ email }">초대 수락</a><br/>
+      <a href="http://${process.env.REACT_APP_RESTAPI_IP}/invitedMemberSignup/${ projectInfo.projectCode }/${ email }">초대 수락</a><br/>
       해당 링크는 48시간동안 유효합니다.<br/>
       <br/>
       <br/>
